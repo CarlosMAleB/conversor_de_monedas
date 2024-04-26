acf5bdae61047629051a6f8a9391ed9873e4e9c2
Carlos Ale <alecarlos85@gmail.com>
2024-04-26 09:42:17 -0300

    firts commit

---
+++ src/com/alura/conversor/moneda/TasaConversion.java
@@ -0,0 +1,11 @@
+package com.alura.conversor.moneda;
+
+public class TasaConversion {
+
+
+    public static final double TASA_CONVERSION_DOLAR = 961.64;
+    public static final double TASA_CONVERSION_EURO = 1052.07;
+    public static final double TASA_CONVERSION_LIBRA = 1235.83;
+    public static final double TASA_CONVERSION_YEN = 6.54;
+    public static final double TASA_CONVERSION_WON_COREANO = 0.73;
+}
